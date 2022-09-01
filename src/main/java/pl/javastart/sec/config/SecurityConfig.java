5ad7840f37f7b7386af01a8bb985d7272b1eda3f
olo111 <sagital@interia.pl>
2022-09-01 16:26:37 +0200

    cativate commit

---
+++ src/main/java/pl/javastart/sec/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package pl.javastart.sec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/register", "/confirmation").permitAll()
+                .mvcMatchers("/secured").hasRole("USER")
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        );
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+        http.csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().mvcMatchers(
+                "/img/**",
+                "/styles/**"
+        );
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
